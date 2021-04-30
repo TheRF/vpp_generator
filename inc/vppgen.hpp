cdd47932c12e1fee1d1804e09f49afac96fa3095
r_fast <r_fast@web.de>
2021-04-30 11:43:57 +0200

    js->c++

---
+++ inc/vppgen.hpp
@@ -0,0 +1,9 @@
+#ifndef VPP_GEN_H
+#define VPP_GEN_H
+#include <vector>
+#include <string>
+
+std::vector<std::string> parseAll(const std::vector<std::string> slInputLeft,
+  const std::vector<std::string> slInputRight, bool bClub);
+
+#endif
--- src/ui.js
+++
@@ -1,9 +0,0 @@
-function ShowHideP(chkPara) {
-  var pDeb = document.getElementById("debugPara");
-  pDeb.style.display = chkPara.checked ? "block" : "none";
-}
-
-function SetDebugText(chkPara) {
-  var pDeb = document.getElementById("debugPara");
-  pDeb.innerText = "Test";
-}
