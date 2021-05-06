758aa887c733d5aee04d73fc4633e9919e8aa2db
rfast <r_fast@web.de>
2021-05-06 17:52:05 +0200

    inifile window pos

--- src/dlgmain.pas
+++ src/dlgmain.pas
@@ -9,9 +9,9 @@ uses
 
 type
 
-  { TForm1 }
+  { TVppGenForm }
 
-  TForm1 = class(TForm)
+  TVppGenForm = class(TForm)
     btnGenerate: TButton;
     cbDebug: TCheckBox;
     Label1: TLabel;
@@ -23,6 +23,8 @@ type
     Panel2: TPanel;
     Panel3: TPanel;
     procedure btnGenerateClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
 
   public
@@ -30,17 +32,69 @@ type
   end;
 
 var
-  Form1: TForm1;
+  VppGenForm: TVppGenForm;
 
 implementation
 
 {$R *.lfm}
 
-{ TForm1 }
+uses
+  Inifiles;
 
-procedure TForm1.btnGenerateClick(Sender: TObject);
+{ TVppGenForm }
+
+procedure TVppGenForm.btnGenerateClick(Sender: TObject);
 begin
   //
+end;
+
+const
+  APP_SEC = 'General';
+  APP_TOP = 'top';
+  APP_LEFT = 'left';
+  APP_WIDTH = 'width';
+  APP_HEIGHT = 'height';
+
+  INI_EXT = '.ini';
+
+procedure TVppGenForm.FormCreate(Sender: TObject);
+var
+  sIni: string;
+  iniConfig: TIniFile;
+begin
+  //properly position the window
+  sIni := ApplicationName()+INI_EXT;
+
+  iniConfig := TIniFile.Create(sIni);
+  try
+    Top := iniConfig.ReadInteger(APP_SEC, APP_TOP, Top);
+    Left := iniConfig.ReadInteger(APP_SEC, APP_LEFT, LEFT);
+    Width := iniConfig.ReadInteger(APP_SEC, APP_WIDTH, WIDTH);
+    Height := iniConfig.ReadInteger(APP_SEC, APP_HEIGHT, HEIGHT);
+  finally
+    FreeAndNil(iniConfig);
+  end;
+
+  MakeFullyVisible();
+end;
+
+procedure TVppGenForm.FormDestroy(Sender: TObject);
+var
+  sIni: string;
+  iniConfig: TIniFile;
+begin
+  //save the window position
+  sIni := ApplicationName()+INI_EXT;
+
+  iniConfig := TIniFile.Create(sIni);
+  try
+    iniConfig.WriteInteger(APP_SEC, APP_TOP, Top);
+    iniConfig.WriteInteger(APP_SEC, APP_LEFT, LEFT);
+    iniConfig.WriteInteger(APP_SEC, APP_WIDTH, WIDTH);
+    iniConfig.WriteInteger(APP_SEC, APP_HEIGHT, HEIGHT);
+  finally
+    FreeAndNil(iniConfig);
+  end;
 end;
 
 end.
